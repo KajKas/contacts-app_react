76358c6bcdc6b2bdd3535c00bd407f84583a682a
Kajetan Kasperuk <kajetankasperuk@gmail.com>
2018-08-08 21:05:04 +0200

    inputs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type={'text'} placeholder={'first name'} />
+        <input type={'text'} placeholder={'last name'} />
+        <input type={'number'} placeholder={'phone number'} />
+        <input type={'email'} placeholder={'email address'} />
+        <button>Add</button>
+        <ul></ul>
       </div>
     );
   }
