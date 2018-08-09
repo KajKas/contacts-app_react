039353c2c118eb283fd4ab2814cfeacce860a76a
Kajetan Kasperuk <kajetankasperuk@gmail.com>
2018-08-09 21:29:18 +0200

    still won't add contacts

--- src/App.js
+++ src/App.js
@@ -26,15 +26,24 @@ class App extends Component {
     })
   }
 
+  handleSubmit = event => {
+    event.preventDefault()
+    this.props.addContact(this.state.firstName, this.state.lastName, this.state.phoneNumber, this.state.email)
+
+  }
+
+
 
   render() {
     return (
       <div className="App">
-        <input type='text' placeholder='first name' onChange={(event) => this.setState({firstName: event.target.value})}/>
-        <input type='text' placeholder='last name' />
-        <input type='number' placeholder='phone number' />
-        <input type='email' placeholder='email address' />
-        <button onClick={() => this.addContact(this.props.firstName, 'dupa', '666', 'ddd@vb.pl')}>Add</button>
+        <form onSubmit={this.handleSubmit}>
+          <input type='text' placeholder='first name' value={this.state.firstName} onChange={(event) => this.setState({firstName: event.target.value})}/>
+          <input type='text' placeholder='last name' value={this.state.lastName} onChange={(event) => this.setState({lastName: event.target.value})}/>
+          <input type='number' placeholder='phone number' value={this.state.phoneNumber} onChange={(event) => this.setState({phoneNumber: event.target.value})}/>
+          <input type='email' placeholder='email address' value={this.state.email} onChange={(event) => this.setState({email: event.target.value})}/>
+          <button onClick={() => this.addContact(this.props.firstName, this.props.lastName, this.props.phoneNumber, this.props.email)}>Add</button>
+        </form>
         <ul>
           {
             this.state.contacts.map(
