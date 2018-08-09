b1ee4a294aa7ffc6f7d903f606a2cee9a36a567f
Kajetan Kasperuk <kajetankasperuk@gmail.com>
2018-08-09 17:21:46 +0200

    displaying contacts doesn't work :(

--- src/App.js
+++ src/App.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    contacts: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3004/contacts')
+      .then(response => response.json())
+      .then(contacts => this.setState({
+        contacts: [
+          {
+            firstName: contacts.firstName,
+            lastName: contacts.lastName,
+            phoneNumber: contacts.phoneNumber,
+            emailAddress: contacts.emailAddress
+          }
+        ]
+      }));
+  }
+
+
+
   render() {
     return (
       <div className="App">
@@ -10,9 +32,22 @@ class App extends Component {
         <input type={'number'} placeholder={'phone number'} />
         <input type={'email'} placeholder={'email address'} />
         <button>Add</button>
-        <ul></ul>
+        <ul>
+          {
+            this.state.contacts.map(
+              contact => (
+                <li>
+                  first name: {contact.firstName}
+                  last name: {this.state.contacts.lastName}
+                  phone number: {this.phoneNumber}
+                  email address: {this.emailAddress}
+                </li>
+              )
+            )
+          }
+        </ul>
       </div>
-    );
+    )
   }
 }
 
