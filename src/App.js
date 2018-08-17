412da8bbf84a4634fcd6dd7d0900590d5a7d5766
Kajetan Kasperuk <kajetankasperuk@gmail.com>
2018-08-17 13:28:21 +0200

    ok, now clear works

--- src/App.js
+++ src/App.js
@@ -77,10 +77,10 @@ class App extends Component {
     return (
       <div className="App">
         <form onSubmit={this.handleSubmit}>
-          <input type='text' placeholder='first name' onChange={(event) => this.setState({firstName: event.target.value})}/>
-          <input type='text' placeholder='last name' onChange={(event) => this.setState({lastName: event.target.value})}/>
-          <input type='number' placeholder='phone number' onChange={(event) => this.setState({phoneNumber: event.target.value})}/>
-          <input type='email' placeholder='email address' onChange={(event) => this.setState({email: event.target.value})}/>
+          <input type='text' placeholder='first name' value={this.state.firstName} onChange={(event) => this.setState({firstName: event.target.value})}/>
+          <input type='text' placeholder='last name' value={this.state.lastName} onChange={(event) => this.setState({lastName: event.target.value})}/>
+          <input type='number' placeholder='phone number' value={this.state.phoneNumber} onChange={(event) => this.setState({phoneNumber: event.target.value})}/>
+          <input type='email' placeholder='email address' value={this.state.email} onChange={(event) => this.setState({email: event.target.value})}/>
           <button>Add</button>
         </form>
         <ul>
