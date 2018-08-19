713db33750cd52c04dc9e0617ea53d59ff1a1901
Kajetan Kasperuk <kajetankasperuk@gmail.com>
2018-08-19 10:44:26 +0200

    trying to edit

--- src/App.js
+++ src/App.js
@@ -73,6 +73,36 @@ class App extends Component {
     ).then(sync)
   }
 
+  handleEdit = (contactId, event) => {
+    event.preventDefault()
+    this.clearInputs()
+    this.editContact(
+      contactId,
+      this.state.firstName,
+      this.state.lastName,
+      this.state.phoneNumber,
+      this.state.email
+    )
+  }
+
+  editContact = (contactId, contactFirstName, contactLastName, contactNumber, contactEmail) => {
+    const sync = () => this.getContacts()
+    fetch(
+      'http://localhost:3004/contacts/' + contactId, {
+        method: 'PATCH',
+        body: JSON.stringify({
+          firstName: contactFirstName,
+          lastName: contactLastName,
+          phoneNumber: contactNumber,
+          email: contactEmail
+        }),
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(sync)
+  }
+
   render() {
     return (
       <div className="App">
@@ -93,7 +123,14 @@ class App extends Component {
                   phone number: {contact.phoneNumber},
                   email address: {contact.email}
                   <button onClick={() => this.handleDelete(contact.id)}>x</button>
-                  <button>edit</button>
+                  <button onClick={() => this.handleEdit(contact.id)}>edit</button>
+                  <form>
+                    <input type='text' placeholder='first name' value={this.state.firstName} onChange={(event) => this.setState({firstName: event.target.value})}/>
+                    <input type='text' placeholder='last name' value={this.state.lastName} onChange={(event) => this.setState({lastName: event.target.value})}/>
+                    <input type='number' placeholder='phone number' value={this.state.phoneNumber} onChange={(event) => this.setState({phoneNumber: event.target.value})}/>
+                    <input type='email' placeholder='email address' value={this.state.email} onChange={(event) => this.setState({email: event.target.value})}/>
+                    <button>Save</button>
+                  </form>
                 </li>
               )
             )
